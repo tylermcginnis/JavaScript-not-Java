40a9da10346b1d0f794d97ee6e047850bfb732b7
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-01-11 14:58:41 -0700

    add Images and grid

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,18 @@ angular.module('jvj.controllers', [])
     $scope.test = 'testtest';
   }])
   .controller('homePageListings', ['$scope', function($scope){
-    $scope.test = 'in home page listings view'
+    $scope.imageArray = [
+        'images/logos/c-logo.jpg',
+        'images/logos/cpp-logo.jpeg',
+        'images/logos/csharp-logo.png',
+        'images/logos/css3-logo.png',
+        'images/logos/html5-logo.png',
+        'images/logos/java-logo.png',
+        'images/logos/js-logo.png',
+        'images/logos/objective-c-logo.png',
+        'images/logos/perl-logo.JPG',
+        'images/logos/php-logo.png',
+        'images/logos/python-logo.jpg',
+        'images/logos/ruby-logo.png',
+    ];
   }])
